190af4d82d3bcd361716f2e793f6b9ba5fd72e52
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2026-06-19 20:19:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Challege App with AWS and Spring Boot
+a simple project to practice AWS and Spring boot
---
+++ src/main/java/com/chunyinng078/ChallengeApp/ChallengeController.java
@@ -0,0 +1,8 @@
+package com.chunyinng078.ChallengeApp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChallengeController {
+    private List<Challenge> challenges = new ArrayList<>();
+}
