e122a9ef7bf649e8d87eab31a7db5b9a6ada62a7
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2026-06-20 00:32:40 +0800

    Get Method for getting single challange

--- src/main/java/com/chunyinng078/ChallengeApp/ChallengeService.java
+++ src/main/java/com/chunyinng078/ChallengeApp/ChallengeService.java
@@ -1,5 +1,40 @@
 package com.chunyinng078.ChallengeApp;
 
+import java.util.List;
+import java.util.ArrayList;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ChallengeService {
-    
+
+    private List<Challenge> challenges = new ArrayList<>();
+
+    private Long nextId =0L;
+
+    public ChallengeService() {
+
+    }
+
+    public List<Challenge> getAllChallenges() {
+        return challenges;
+    }
+
+    public Challenge getChallenges(String month) {
+        for (Challenge challenge:challenges){
+            if(challenge.getMonth().equals(month)){
+                return challenge;
+            }
+        }
+        return null;
+    }
+
+    public boolean addChallenge(Challenge challenge) {
+        if (challenge != null){
+            challenge.setId(nextId++);
+            challenges.add(challenge);
+            return true;
+        }
+        return false;
+        
+    }
 }
