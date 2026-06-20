2e589b26db598b06929f934f0c4bc81450af99c5
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2026-06-20 19:07:13 +0800

    remove spaces

--- src/main/java/com/chunyinng078/ChallengeApp/ChallengeRepository.java
+++ src/main/java/com/chunyinng078/ChallengeApp/ChallengeRepository.java
@@ -1,11 +1,11 @@
 package com.chunyinng078.ChallengeApp;
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChallengeRepository extends JpaRepository<Challenge, Long>{
+public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
     Optional<Challenge> findByMonthIgnoreCase(String month);
 
-    
 }
