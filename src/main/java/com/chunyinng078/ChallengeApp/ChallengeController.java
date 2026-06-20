c8c935d4d1138b76d1e16dd0b79881d5ea70669a
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2026-06-20 16:37:41 +0800

    Put request

--- src/main/java/com/chunyinng078/ChallengeApp/ChallengeController.java
+++ src/main/java/com/chunyinng078/ChallengeApp/ChallengeController.java
@@ -3,13 +3,15 @@ package com.chunyinng078.ChallengeApp;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 public class ChallengeController {
@@ -35,9 +37,19 @@ public class ChallengeController {
 
     @GetMapping("/challenges/{month}")
     public ResponseEntity<Challenge> getAChallenge(@PathVariable String month) {
-        Challenge challenge = challengeService.getChallenges(month);
+        Challenge challenge = challengeService.getChallenge(month);
         if (challenge != null)
             return new ResponseEntity<Challenge>(challenge, HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/challenges/{id}")
+    public ResponseEntity<String> updateChallenge(@PathVariable Long id, @RequestBody Challenge updatedChallenge) {
+        boolean isChallengeUpdated = challengeService.updateChallenge(id, updatedChallenge);
+        if (isChallengeUpdated) {
+            return new ResponseEntity<>("Challenge updated successfully", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Challenge updates fails", HttpStatus.NOT_FOUND);
+
     }
 }
--- src/main/java/com/chunyinng078/ChallengeApp/ChallengeService.java
+++ src/main/java/com/chunyinng078/ChallengeApp/ChallengeService.java
@@ -19,9 +19,9 @@ public class ChallengeService {
         return challenges;
     }
 
-    public Challenge getChallenges(String month) {
-        for (Challenge challenge:challenges){
-            if(challenge.getMonth().equals(month)){
+    public Challenge getChallenge(String month) {
+        for (Challenge challenge : challenges) {
+            if (challenge.getMonth() != null && challenge.getMonth().equalsIgnoreCase(month)) {
                 return challenge;
             }
         }
@@ -29,12 +29,23 @@ public class ChallengeService {
     }
 
     public boolean addChallenge(Challenge challenge) {
-        if (challenge != null){
+        if (challenge != null) {
             challenge.setId(nextId++);
             challenges.add(challenge);
             return true;
         }
         return false;
-        
+
+    }
+
+    public boolean updateChallenge(Long id, Challenge updatedChallenge) {
+        for (Challenge challenge : challenges) {
+            if (challenge.getId().equals(id)) {
+                challenge.setMonth(updatedChallenge.getMonth());
+                challenge.setDescription(updatedChallenge.getDescription());
+                return true;
+            }
+        }
+        return false;
     }
 }
